109fa323384065fddf10886acd3d6f9fc16ebeb3
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2025-02-18 23:03:57 +0100

    feat(frame): add @farcaster/frame-sdk

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import FrameSDK from '@farcaster/frame-sdk'
+import { useEffect } from 'react'
+
+import { ReactNode } from 'react'
+
+function FarcasterFrameProvider({ children }: { children: ReactNode }) {
+  useEffect(() => {
+    const init = async () => {
+      setTimeout(() => {
+        FrameSDK.actions.ready()
+      }, 500)
+    }
+    init()
+  }, [])
+  return <>{children}</>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FarcasterFrameProvider>
+      <App />
+    </FarcasterFrameProvider>
   </StrictMode>,
 )
